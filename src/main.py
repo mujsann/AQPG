c2d3c9b9b59b329da6fd376d9967f74244009243
Gboye <mujsann@gmail.com>
2021-06-11 10:03:03 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from src.segmentation import Segmentation
 from src.extraction import Extraction
 from src.data.entities import Story, Entities
 
-# Calls the segmentation class and passes the story and entities to it
 a = Segmentation(Story, Entities).create()
 print("\n \n \n Extraction...")
 Extraction(a).create()
