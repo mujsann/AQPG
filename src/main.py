d4453e6727ffeddd3e759340064a61ad8bc5869e
Gboye <mujsann@gmail.com>
2021-06-11 10:02:42 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,19 +8,3 @@ print("\n \n \n Extraction...")
 Extraction(a).create()
 
 
-# import os
-# import subprocess
-#
-# heuristic = "astar(ipdb())"
-# def plan_quest():
-#
-#     with open("garbage","w") as garbage:
-#         subprocess.call(["/Users/GBOYEs/PycharmProjects/conan-procedural-quest-generation-master/downward/src/translate/translate.py","domain.pddl", "problem.pddl"], stdout=garbage)
-#     with open("output.sas","r") as output:
-#         with open(os.path.join("","quest.soln"),"w") as solution:
-#             calc = subprocess.Popen(["/Users/GBOYEs/PycharmProjects/conan-procedural-quest-generation-master/downward/fast-downward.py","domain.pddl", "problem.pddl", '--search', heuristic], stdin=output, stdout=solution)
-#     os.chdir("..")
-#     os.chdir("..")
-#     return calc
-#
-# plan_quest()
